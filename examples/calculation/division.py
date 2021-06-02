221cbc659c3a42b77055b3f6e7045eeb81ebd389
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-06-02 12:20:14 +0530

    Create division.py

---
+++ examples/calculation/division.py
@@ -0,0 +1,23 @@
+# Author: Fayas (https://github.com/FayasNoushad) (@FayasNoushad)
+
+>>> 6 / 2
+3.0
+
+>>> a = 8
+>>> b = 2
+>>> a / b
+4.0
+
+>>> a = int("10")
+>>> b = int("2")
+>>> a / b
+5.0
+
+>>> 5 / 2
+2.5
+
+>>> 8 // 2
+4
+
+>>> round(10 / 2)
+5
